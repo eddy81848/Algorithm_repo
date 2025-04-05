f435a3b738024b7809ee1e0806709a4d5d01d7d2
eddy81848 <eddy81848@gmail.com>
2025-04-05 10:07:36 +0900

    solve 1976, 6549, 17480

--- src/BOJ2457.java
+++ src/BOJ2457.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class BOJ2457 {
     public static void main(String[] args) throws IOException {
@@ -6,6 +7,10 @@ public class BOJ2457 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int[][] flowers = new int[n][2];
+        int pointer = 0;
+        int start = 301;
+        int cnt = 0;
+        boolean flag = false;
         for (int i = 0; i < n; i++) {
             String st = br.readLine();
             String[] tokens = st.split(" ");
@@ -17,6 +22,39 @@ public class BOJ2457 {
             flowers[i][0] = Integer.parseInt(tokens[0] + tokens[1]);
             flowers[i][1] = Integer.parseInt(tokens[2] + tokens[3]);
         }
-        
+
+        Arrays.sort(flowers, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        while (start <= 1130) {
+            flag = false;
+            int max = Integer.MIN_VALUE;
+            for (int i = pointer; i < n; i++) {
+                if (flowers[i][0] > start) break;
+                if (flowers[i][1] > max) {
+                    max = flowers[i][1];
+                    pointer = i + 1;
+                    flag = true;
+                }
+            }
+            if (flag) {
+                start = max;
+                cnt++;
+            } else {
+                break;
+            }
+        }
+        if (flag) {
+            bw.write(Integer.toString(cnt));
+        } else {
+            bw.write("0");
+        }
+        bw.flush();
+        bw.close();
     }
 }
--- src/BOJ3015.java
+++ src/BOJ3015.java
@@ -1,22 +1,40 @@
 import java.io.*;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 
 public class BOJ3015 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new LinkedList<>();
-        int sum = 0;
+        int cnt = 0;
+        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.addLast(Integer.MAX_VALUE);
         for (int i = 0; i < n; i++) {
             list.add(Integer.parseInt(br.readLine()));
         }
-        Iterator<Integer> iterator = list.iterator();
+        list.addLast(Integer.MAX_VALUE);
 
-        
-        bw.write(Integer.toString(sum));
+        for (int i : list) {
+            if (!stack.isEmpty()) {
+                int peek = stack.peekLast();
+                if (peek < i) {
+                    while (!stack.isEmpty() && stack.peekLast() < peek) {
+                        int top = stack.removeLast();
+                        /*
+                        마지막에 넣은 것 보다 큰 것이 들어올 경우 처리
+                        새로운 블록과 높이가 크거나 같을 때까지 pop
+                        */
+
+
+                    }
+                } else {
+                    stack.addLast(i);
+                }
+            }
+        }
+
+        bw.write(Integer.toString(cnt));
         bw.flush();
         bw.close();
     }
